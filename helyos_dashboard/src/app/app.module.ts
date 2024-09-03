a74ac0b393e327431176285f9838f42ba99ca948
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-03 15:56:59 +0200

    fix lint app.module

--- helyos_dashboard/src/app/app.module.ts
+++ helyos_dashboard/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
