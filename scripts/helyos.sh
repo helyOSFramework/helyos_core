0350ddc81a3bda0fffe96e236ae0f95bf4479fe6
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-04 18:04:35 +0200

    reset_dir func

--- scripts/helyos.sh
+++ scripts/helyos.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# reset dir func
 set -e
+
+reset_dir() {
+    local dir=$1
+    
+    [ -d $dir ] && sudo rm -rf $dir
+    sudo mkdir -p $dir
+}
 
 helyos_database=$working_dir/helyos_database
 helyos_dashboard=$working_dir/helyos_dashboard
@@ -24,6 +32,7 @@ usr_local_helyos_bin=$usr_local_helyos_core/bin
 usr_local_helyos_bin_postgres=$usr_local_helyos_bin/wait-for-postgres.sh
 usr_local_helyos_bin_entrypoint=$usr_local_helyos_bin/entrypoint.sh
 
+# fix these folders paths
 usr_local_helyos_database=$usr_local_helyos_core/helyos_database
 usr_local_helyos_server=$usr_local_helyos_core/helyos_server
 usr_local_helyos_dashboard_dist=$usr_local_helyos_core/helyos_dashboard/dist
@@ -34,25 +43,21 @@ etc_helyos_config=$etc_helyos/config
 etc_helyos_db_initial_data=$etc_helyos/db_initial_data
 etc_helyos_ssl_keys=$etc_helyos/.ssl_keys
 
-[ -d $usr_local_helyos_core ] && sudo rm -rf $usr_local_helyos_core
-sudo mkdir -p $usr_local_helyos_core
+reset_dir $usr_local_helyos_core
 
 # DASHBOARD
-[ -d $usr_src_app ] && sudo rm -rf $usr_src_app
-sudo mkdir -p $usr_src_app
+reset_dir $usr_src_app
 sudo cp -r $helyos_dashboard/* $usr_src_app
 cd $usr_src_app
 sudo npm set fetch-retry-maxtimeout 600000 && sudo npm install --no-audit --timeout=600000
 sudo npm run build --prod
-[ -d $usr_local_helyos_dashboard_dist ] && sudo rm -rf $usr_local_helyos_dashboard_dist
-sudo mkdir -p $usr_local_helyos_dashboard_dist
+reset_dir $usr_local_helyos_dashboard_dist
 sudo cp -r $usr_src_app_dist/* $usr_local_helyos_dashboard_dist
 
 sudo cp $helyos_license $usr_local_helyos_license
 
 # HELYOS CORE SERVER
-[ -d $usr_local_helyos_server ] && sudo rm -rf $usr_local_helyos_server
-sudo mkdir -p $usr_local_helyos_server
+reset_dir $usr_local_helyos_server
 sudo cp -r $helyos_server/* $usr_local_helyos_server
 cd $usr_local_helyos_server
 sudo npm ci --omit=dev --timeout=600000
@@ -61,8 +66,7 @@ echo $PGHOST $PGPASSWORD $PGUSER $PGDATABASE $RUN_MODE
 export PGHOST PGPASSWORD PGUSER PGDATABASE RUN_MODE
 
 # DATABASE
-[ -d $usr_local_helyos_database ] && sudo rm -rf $usr_local_helyos_database
-sudo mkdir -p $usr_local_helyos_database
+reset_dir $usr_local_helyos_database
 sudo cp -r $helyos_database/* $usr_local_helyos_database
 
 sudo dos2unix /usr/local/helyos_core/helyos_database/db_commands/migrate.sh
@@ -74,8 +78,7 @@ sudo chmod +x /usr/local/helyos_core/helyos_database/db_commands/create_admin_ac
 bash /usr/local/helyos_core/helyos_database/db_commands/create_admin_account.sh
 
 # SETTINGS
-[ -d $etc_helyos ] && sudo rm -rf $etc_helyos
-sudo mkdir -p $etc_helyos
+reset_dir $etc_helyos
 sudo cp -r $demo_settings_config $etc_helyos_config
 sudo cp -r $demo_settings_db_initial_data $etc_helyos_db_initial_data
 sudo cp -r $demo_settings_rsa_keys $etc_helyos_ssl_keys
@@ -83,8 +86,7 @@ sudo chmod -R a-w,u+r $etc_helyos_ssl_keys/*
 
 # ENTRYPOINT FILES
 
-[ -d $usr_local_helyos_bin ] && sudo rm -rf $usr_local_helyos_bin
-sudo mkdir -p $usr_local_helyos_bin
+reset_dir $usr_local_helyos_bin
 
 sudo cp $packaging_postgres $usr_local_helyos_bin_postgres
 sudo dos2unix $usr_local_helyos_bin_postgres
