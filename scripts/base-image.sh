5392f92dc46b995d98903af9e4d4fe1752400f6a
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-09 18:39:15 +0200

    add color for output

--- scripts/base-image.sh
+++ scripts/base-image.sh
@@ -2,12 +2,21 @@
 
 set -e
 
+# Define color codes
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}### Updating package list...${NC}"
 sudo apt-get -q update
+
+echo -e "${GREEN}### Installing curl and dos2unix...${NC}"
 sudo apt-get -yq install curl dos2unix
 
-# Install Node.js 18
+echo -e "${GREEN}### Installing Node.js 18...${NC}"
 curl -sSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get -yq install nodejs
 
-# Install PostgreSQL and postgresql-contrib
+echo -e "${GREEN}### Installing PostgreSQL and postgresql-contrib...${NC}"
 sudo apt-get -yq install postgresql-client-16 postgresql-contrib-16
+
+echo -e "${GREEN}### All installations completed successfully.${NC}"
