03189de664a254bc9ad70c2bd02e6dbca7e4faf3
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-09 18:01:10 +0200

    refactor

--- scripts/helyos.sh
+++ scripts/helyos.sh
@@ -2,38 +2,43 @@
 
 set -e
 
-# FUNCTIONS
+# region FUNCTIONS
 delete_dir() {
     local dir=$1
     
-    [ -d $dir ] && sudo rm -rf $dir
+    if [ -d "$dir" ]; then
+        sudo rm -rf "$dir"
+    fi
 }
 
 reset_dir() {
     local dir=$1
     
-    [ -d $dir ] && sudo rm -rf $dir
+    delete_dir $dir
     sudo mkdir -p $dir
 }
+# endregion
 
 reset_dir /usr/local/helyos_core
 sudo cp LICENSE.txt /usr/local/helyos_core/LICENSE.txt
 
-# DASHBOARD
+# region DASHBOARD
 reset_dir /usr/src/app
 sudo cp -r helyos_dashboard/* /usr/src/app
 sudo npm ci --prefer-offline --no-audit --no-fund --prefix=/usr/src/app
 sudo npm run build --prod --prefix=/usr/src/app
 reset_dir /usr/local/helyos_core/helyos_dashboard/dist
 sudo cp -r /usr/src/app/dist/* /usr/local/helyos_core/helyos_dashboard/dist
+# endregion
 delete_dir /usr/src/app
 
-# HELYOS CORE SERVER
+# region HELYOS CORE SERVER
 reset_dir /usr/local/helyos_core/helyos_server
 sudo cp -r helyos_server/* /usr/local/helyos_core/helyos_server
 sudo npm ci --prefer-offline --no-audit --no-fund --prefix=/usr/local/helyos_core/helyos_server
+# endregion
 
-# DATABASE
+# region DATABASE
 echo $PGHOST $PGPASSWORD $PGUSER $PGDATABASE $RUN_MODE
 export PGHOST PGPASSWORD PGUSER PGDATABASE RUN_MODE
 
@@ -47,15 +52,17 @@ bash /usr/local/helyos_core/helyos_database/db_commands/migrate.sh
 sudo dos2unix /usr/local/helyos_core/helyos_database/db_commands/create_admin_account.sh
 sudo chmod +x /usr/local/helyos_core/helyos_database/db_commands/create_admin_account.sh
 bash /usr/local/helyos_core/helyos_database/db_commands/create_admin_account.sh
+# endregion
 
-# SETTINGS
+# region SETTINGS
 reset_dir /etc/helyos
 sudo cp -r demo/settings/config /etc/helyos/config
 sudo cp -r demo/settings/db_initial_data /etc/helyos/db_initial_data
 sudo cp -r demo/settings/rsa_keys /etc/helyos/.ssl_keys
 sudo chmod -R a-w,u+r /etc/helyos/.ssl_keys/*
+# endregion
 
-# ENTRYPOINT FILES
+# region ENTRYPOINT FILES
 reset_dir /usr/local/helyos_core/bin
 
 sudo cp packaging/wait-for-postgres.sh /usr/local/helyos_core/bin/wait-for-postgres.sh
@@ -67,3 +74,4 @@ sudo cp packaging/entrypoint.sh /usr/local/helyos_core/bin/entrypoint.sh
 sudo dos2unix /usr/local/helyos_core/bin/entrypoint.sh
 sudo chmod +x /usr/local/helyos_core/bin/entrypoint.sh
 bash /usr/local/helyos_core/bin/entrypoint.sh
+# endregion
