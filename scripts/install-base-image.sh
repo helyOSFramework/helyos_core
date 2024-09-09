11212e06d1a9064db5444de4d6d2186dd0ecbf21
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-10 01:13:03 +0200

    refactor

--- scripts/install-base-image.sh
+++ scripts/install-base-image.sh
@@ -6,17 +6,21 @@ set -e
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}### Updating package list...${NC}"
+print() {
+    echo -e "\n${GREEN}### $1...${NC}"
+}
+
+print "Updating package list"
 sudo apt-get -q update
 
-echo -e "${GREEN}### Installing curl and dos2unix...${NC}"
+print "Installing curl and dos2unix"
 sudo apt-get -yq install curl dos2unix
 
-echo -e "${GREEN}### Installing Node.js 18...${NC}"
+print "Installing Node.js 18"
 curl -sSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get -yq install nodejs
 
-echo -e "${GREEN}### Installing PostgreSQL and postgresql-contrib...${NC}"
+print "Installing PostgreSQL and postgresql-contrib"
 sudo apt-get -yq install postgresql-client-16 postgresql-contrib-16
 
-echo -e "${GREEN}### All installations completed successfully.${NC}"
+print "All installations completed successfully"
