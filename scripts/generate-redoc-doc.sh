e54ceced0ca5c7ea72e1d0171292d998456596a2
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-10 12:01:07 +0200

    refactor

--- scripts/generate-redoc-doc.sh
+++ scripts/generate-redoc-doc.sh
@@ -2,10 +2,17 @@
 
 set -e
 
-cd /usr/local/helyos_core/helyos_server
+helyos_server_path=/usr/local/helyos_core/helyos_server
+
+if [ ! -d "$helyos_server_path" ]; then
+  echo "Error: Directory $helyos_server_path does not exist."
+  exit 1
+fi
+
+cd $helyos_server_path
 
 # generate redoc static api documentation instead of using swagger on the fly
-
-npm install redoc-cli
+npm install --no-save redoc-cli
 npm run make_map_api_doc
 npm run make_path_api_doc
+npm uninstall redoc-cli
--- scripts/source-to-bin.sh
+++ scripts/source-to-bin.sh
@@ -3,9 +3,15 @@
 set -e
 
 helyos_server_path=/usr/local/helyos_core/helyos_server
+
+if [ ! -d "$helyos_server_path" ]; then
+  echo "Error: Directory $helyos_server_path does not exist."
+  exit 1
+fi
+
 cd $helyos_server_path
 
-npm install pkg
+npm install --no-save pkg
 npx pkg --o $helyos_server_path/helyos_core.app --targets node18-linuxstatic-x64 $helyos_server_path/src/main.js 
 chmod +x $helyos_server_path/helyos_core.app
 npm uninstall pkg
