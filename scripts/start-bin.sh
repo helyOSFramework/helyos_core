f4e2a86f159ace2b42cf3bb196c9690eaa5b8a91
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-10 15:10:01 +0200

    start binary

---
+++ scripts/start-bin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d "/usr/local/helyos_core/helyos_server" ]; then
+  echo "Error: Directory /usr/local/helyos_core/helyos_server does not exist."
+  exit 1
+fi
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+    set -o allexport
+    source .env
+    set +o allexport
+fi
+
+
+cd /usr/local/helyos_core/helyos_server
+/usr/local/helyos_core/helyos_server/helyos_core.app
