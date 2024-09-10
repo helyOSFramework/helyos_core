f9363975d8733dc32567ca41bf18acb44075b5ee
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-10 16:28:44 +0200

    refactor

--- scripts/install-helyos.sh
+++ scripts/install-helyos.sh
@@ -19,75 +19,78 @@ reset_dir() {
 }
 # endregion
 
-print_success "Resetting /usr/local/helyos_core"
+# region DIRECTORY SETUP
 reset_dir /usr/local/helyos_core
-sudo cp LICENSE.txt /usr/local/helyos_core/LICENSE.txt
+reset_dir /usr/src/app
+reset_dir /usr/local/helyos_core/helyos_dashboard/dist
+reset_dir /usr/local/helyos_core/helyos_server
+reset_dir /usr/local/helyos_core/helyos_database
+reset_dir /etc/helyos
+reset_dir /usr/local/helyos_core/bin
+
+print_success "Changing ownership of /usr/local/helyos_core /usr/src/app /etc/helyos to current user"
+sudo chown -R $USER:$USER /usr/local/helyos_core /usr/src/app /etc/helyos
+# endregion
+
+print_success "Resetting /usr/local/helyos_core"
+cp LICENSE.txt /usr/local/helyos_core/LICENSE.txt
 
 # region DASHBOARD
 print_success "Setting up helyos_dashboard"
-reset_dir /usr/src/app
-sudo cp -r helyos_dashboard/* /usr/src/app
+cp -r helyos_dashboard/* /usr/src/app
 print_success "Running npm ci for helyos_dashboard"
-sudo npm ci --prefer-offline --no-audit --no-fund --loglevel=error --prefix=/usr/src/app
+npm ci --prefer-offline --no-audit --no-fund --loglevel=error --prefix=/usr/src/app
 print_success "Building helyos_dashboard"
-sudo npm run build --prod --prefix=/usr/src/app
+npm run build --prod --prefix=/usr/src/app
 print_success "Resetting helyos_dashboard dist directory"
-reset_dir /usr/local/helyos_core/helyos_dashboard/dist
 print_success "Copying built files to helyos_dashboard dist"
-sudo cp -r /usr/src/app/dist/* /usr/local/helyos_core/helyos_dashboard/dist
+cp -r /usr/src/app/dist/* /usr/local/helyos_core/helyos_dashboard/dist
 delete_dir /usr/src/app
 # endregion
 
 # region HELYOS CORE SERVER
 print_success "Setting up helyos_server"
-reset_dir /usr/local/helyos_core/helyos_server
-sudo cp -r helyos_server/* /usr/local/helyos_core/helyos_server
-sudo touch /usr/local/helyos_core/helyos_server/src/microservice_mocks.js
+cp -r helyos_server/* /usr/local/helyos_core/helyos_server
+touch /usr/local/helyos_core/helyos_server/src/microservice_mocks.js
 print_success "Running npm ci for helyos_server"
-sudo npm ci --prefer-offline --no-audit --no-fund --loglevel=error --prefix=/usr/local/helyos_core/helyos_server
+npm ci --prefer-offline --no-audit --no-fund --loglevel=error --prefix=/usr/local/helyos_core/helyos_server
 # endregion
 
 # region DATABASE
 print_success "Setting up helyos_database"
-reset_dir /usr/local/helyos_core/helyos_database
-sudo cp -r helyos_database/* /usr/local/helyos_core/helyos_database
+cp -r helyos_database/* /usr/local/helyos_core/helyos_database
 
 print_success "Converting and setting permissions for migrate.sh"
-sudo dos2unix /usr/local/helyos_core/helyos_database/db_commands/migrate.sh
-sudo chmod +x /usr/local/helyos_core/helyos_database/db_commands/migrate.sh
+dos2unix /usr/local/helyos_core/helyos_database/db_commands/migrate.sh
+chmod +x /usr/local/helyos_core/helyos_database/db_commands/migrate.sh
 
 print_success "Converting and setting permissions for create_admin_account.sh"
-sudo dos2unix /usr/local/helyos_core/helyos_database/db_commands/create_admin_account.sh
-sudo chmod +x /usr/local/helyos_core/helyos_database/db_commands/create_admin_account.sh
+dos2unix /usr/local/helyos_core/helyos_database/db_commands/create_admin_account.sh
+chmod +x /usr/local/helyos_core/helyos_database/db_commands/create_admin_account.sh
 # endregion
 
 # region SETTINGS
 print_success "Copying settings to /etc/helyos"
-reset_dir /etc/helyos
-sudo cp -r demo/settings/config /etc/helyos/config
-sudo cp -r demo/settings/db_initial_data /etc/helyos/db_initial_data
-sudo cp -r demo/settings/rsa_keys /etc/helyos/.ssl_keys
+cp -r demo/settings/config /etc/helyos/config
+cp -r demo/settings/db_initial_data /etc/helyos/db_initial_data
+cp -r demo/settings/rsa_keys /etc/helyos/.ssl_keys
 print_success "Setting permissions for SSL keys"
-sudo chmod -R a-w,u+r /etc/helyos/.ssl_keys/*
+chmod -R a-w,u+r /etc/helyos/.ssl_keys/*
 # endregion
 
 # region ENTRYPOINT FILES
 print_success "Setting up entrypoint files"
-reset_dir /usr/local/helyos_core/bin
 
 print_success "Setting up wait-for-postgres.sh"
-sudo cp packaging/wait-for-postgres.sh /usr/local/helyos_core/bin/wait-for-postgres.sh
-sudo dos2unix /usr/local/helyos_core/bin/wait-for-postgres.sh
-sudo chmod +x /usr/local/helyos_core/bin/wait-for-postgres.sh
+cp packaging/wait-for-postgres.sh /usr/local/helyos_core/bin/wait-for-postgres.sh
+dos2unix /usr/local/helyos_core/bin/wait-for-postgres.sh
+chmod +x /usr/local/helyos_core/bin/wait-for-postgres.sh
 
 print_success "Setting up entrypoint.sh"
-sudo cp packaging/entrypoint.sh /usr/local/helyos_core/bin/entrypoint.sh
-sudo dos2unix /usr/local/helyos_core/bin/entrypoint.sh
-sudo chmod +x /usr/local/helyos_core/bin/entrypoint.sh
+cp packaging/entrypoint.sh /usr/local/helyos_core/bin/entrypoint.sh
+dos2unix /usr/local/helyos_core/bin/entrypoint.sh
+chmod +x /usr/local/helyos_core/bin/entrypoint.sh
 # endregion
-
-print_success "Changing ownership of /usr/local/helyos_core to current user"
-sudo chown -R $USER:$USER /usr/local/helyos_core
 
 print_success "Script completed successfully!"
 
