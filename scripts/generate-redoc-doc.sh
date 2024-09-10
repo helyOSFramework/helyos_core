aee0b01020861ca6fab5928e05bc171b5b669446
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-10 16:13:39 +0200

    refactor

--- scripts/generate-redoc-doc.sh
+++ scripts/generate-redoc-doc.sh
@@ -2,15 +2,19 @@
 
 set -e
 
+source functions.sh
+
 if [ ! -d "/usr/local/helyos_core/helyos_server" ]; then
-  echo "Error: Directory /usr/local/helyos_core/helyos_server does not exist."
+  print_fail "Error: Directory /usr/local/helyos_core/helyos_server does not exist."
   exit 1
 fi
 
 cd /usr/local/helyos_core/helyos_server
 
-# generate redoc static api documentation instead of using swagger on the fly
-npm install --no-save redoc-cli
+print_success "Generating redoc static API documentation"
+
+npm install --prefer-offline --no-audit --no-fund --loglevel=error --no-save redoc-cli
 npm run make_map_api_doc
 npm run make_path_api_doc
-npm uninstall redoc-cli
+
+print_success "API documentation created sucessfully at /usr/local/helyos_core/helyos_server/docs"
--- scripts/source-to-bin.sh
+++ scripts/source-to-bin.sh
@@ -2,19 +2,15 @@
 
 set -e
 
+source functions.sh
+
 if [ ! -d "/usr/local/helyos_core/helyos_server" ]; then
-  echo "Error: Directory /usr/local/helyos_core/helyos_server does not exist."
+  print_fail "Error: Directory /usr/local/helyos_core/helyos_server does not exist."
   exit 1
 fi
 
-source functions.sh
-
-# Export all functions defined in functions.sh
-export -f $(declare -F | awk '{print $3}')
-
 cd /usr/local/helyos_core/helyos_server
 
-# convert the source code to a single binary file
 print_success "Creating binary from helyos_server"
 
 npm install --prefer-offline --no-audit --no-fund --loglevel=error --no-save pkg
