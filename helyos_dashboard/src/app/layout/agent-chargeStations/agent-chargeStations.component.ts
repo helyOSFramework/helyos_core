fbf058f64a25006b891db57ee8445a5eeef6c3bf
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2024-09-20 00:23:45 +0200

    disable object-curly-newline for line

--- helyos_dashboard/src/app/layout/agent-chargeStations/agent-chargeStations.component.ts
+++ helyos_dashboard/src/app/layout/agent-chargeStations/agent-chargeStations.component.ts
@@ -36,10 +36,8 @@ export class AgentChargeStationsComponent implements OnInit {
 
 
   list() {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return this.helyosService.methods.agents.list({
-      agentClass: this.agentClass as any, 
-    })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, object-curly-newline
+    return this.helyosService.methods.agents.list({ agentClass: this.agentClass as any })
       .then(r => this.tools = r);
   }
 
@@ -120,7 +118,7 @@ export class AgentChargeStationsComponent implements OnInit {
         const id = r.id;
         const leaderId = typeof id === 'string' ? Number(id) : id;
         this.helyosService.methods.toolsInterconnections.list({
-          leaderId: leaderId, 
+          leaderId: leaderId,
         })
           .then(r => this.interconnections = r);
       });
@@ -162,7 +160,7 @@ export class AgentChargeStationsComponent implements OnInit {
 
   editItem(item) {
     const patch = {
-      ...item, 
+      ...item,
     };
     delete patch.createdAt;
     delete patch.modifiedAt;
@@ -241,7 +239,7 @@ export class AgentChargeStationsComponent implements OnInit {
     const id = this.selectedItem.id;
     const leaderId = typeof id === 'string' ? Number(id) : id;
     return this.helyosService.methods.toolsInterconnections.list({
-      leaderId: leaderId, 
+      leaderId: leaderId,
     })
       .then(r => this.interconnections = r);
   }
@@ -258,7 +256,7 @@ export class AgentChargeStationsComponent implements OnInit {
     }
 
     this.helyosService.methods.agents.list({
-      uuid: followerUUID, 
+      uuid: followerUUID,
     })
       .then(r => {
         if (!r.length) {
@@ -270,7 +268,7 @@ export class AgentChargeStationsComponent implements OnInit {
         const leaderId = typeof id === 'string' ? Number(id) : id;
         this.helyosService.methods.toolsInterconnections.create({
           followerId: r[0].id,
-          leaderId: leaderId, 
+          leaderId: leaderId,
         })
           .then((r) => {
             if (r.message) {
@@ -291,7 +289,7 @@ export class AgentChargeStationsComponent implements OnInit {
       const promisses = this.interconnections.map(e => this.helyosService.methods.toolsInterconnections.delete(e.id));
       const updtPromises = this.interconnections.map(tool => this.helyosService.methods.agents.patch({
         id: tool.followerId,
-        rbmqUsername: '', 
+        rbmqUsername: '',
       }));
       return Promise.all(promisses).then(() => Promise.all(updtPromises)).then(() => this.interconnectionList());
     }
