2ff18b7b02fa4b076a81e17d3a83fb905998334c
SamiAlavi <sami.mansoor.alavi@ivi.fraunhofer.de>
2025-01-17 20:28:22 +0100

    remove unused vars

--- helyos_dashboard/src/app/mocks/helyos.service.mock.ts
+++ helyos_dashboard/src/app/mocks/helyos.service.mock.ts
@@ -34,22 +34,5 @@ export class MockHelyosService {
       on: jasmine.createSpy('on'),
       removeAllListeners: jasmine.createSpy('removeAllListeners'),
     },
-
-    connectionId: 12345,
-    connected: false,
-    token: 'mockToken',
-    ports: { socketPort: 'mockSocketPort', gqlPort: 'mockGqlPort' },
-    url: 'mockUrl',
-    username: 'mockUsername',
-
-    connect: jasmine.createSpy('connect').and.returnValue(Promise.resolve(true)),
-    register: jasmine.createSpy('register').and.returnValue(Promise.resolve({ id: 'mockId', createdAt: new Date() })),
-    login: jasmine.createSpy('login').and.returnValue(Promise.resolve({ jwtToken: 'mockJwtToken' })),
-    adminGetUserAuthToken: jasmine.createSpy('adminGetUserAuthToken').and.returnValue(Promise.resolve({ jwtToken: 'mockJwtToken' })),
-    changePassword: jasmine.createSpy('changePassword').and.returnValue(Promise.resolve({ success: true })),
-    adminChangePassword: jasmine.createSpy('adminChangePassword').and.returnValue(Promise.resolve({ success: true })),
-    logout: jasmine.createSpy('logout').and.returnValue(Promise.resolve({ msg: 'Logged out successfully' })),
-    convertMMtoLatLng: jasmine.createSpy('convertMMtoLatLng').and.returnValue([]),
-    convertLatLngToMM: jasmine.createSpy('convertLatLngToMM').and.returnValue([]),
   };
 }
